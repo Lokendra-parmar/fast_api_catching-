179a9d45b0ea3bd254e8438233c28b19fa29272d
Lokendra-parmar <lokendrap995@gmail.com>
2026-02-15 02:08:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import time
 from collections import OrderedDict
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 cache = OrderedDict()
 CACHE_LIMIT = 1500
